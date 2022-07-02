ae487215e574f6ce0fa1e680d492117d6c69df05
grandpoisson <grandpoisson@gmail.com>
2022-07-02 13:08:51 +0100

    add 'already applied' button and logic

--- pages/job/[id].js
+++ pages/job/[id].js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
-import { getJob } from 'lib/data'
+import { getJob, alreadyApplied } from 'lib/data'
 import prisma from 'lib/prisma'
+import { getSession } from 'next-auth/react'
 
-export default function Job({ job }) {
+export default function Job({ job, applied }) {
   
   return (
     <div className='flex flex-col w-1/2 mx-auto'>
@@ -20,6 +21,23 @@ export default function Job({ job }) {
       <div className='mb-4 mt-20'>
         <div className='pl-16 pr-16 -mt-6'>
           <p className='text-base font-normal mt-3'>{job.description}</p>
+          {applied ? (
+            <div className='mt-20 flex justify-center '>
+                <Link href={`/dashboard`}>
+                    <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
+                        You already applied!
+                    </button>
+                </Link>
+            </div>
+        ) : (
+            <div className='mt-20 flex justify-center '>
+                <Link href={`/job/${job.id}/apply`}>
+                    <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
+                        Apply to this job
+                    </button>
+                </Link>
+            </div>
+        )}
           <div className='mt-4'>
             <h4 className='inline'>Posted by</h4>
             <div className='inline'>
@@ -38,24 +56,26 @@ export default function Job({ job }) {
           </div>
         </div>
       </div>
-        <div className='mt-20 flex justify-center '>
-            <Link href={`/job/${job.id}/apply`}>
-                <button className=' border  px-8 py-2 mt-0  font-bold rounded-full bg-black text-white '>
-                    Apply to this job
-                </button>
-            </Link>
-        </div>
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
-  let job = await getJob(context.params.id, prisma)
-  job = JSON.parse(JSON.stringify(job))
+    const session = await getSession(context)
+
+    let job = await getJob(context.params.id, prisma)
+    job = JSON.parse(JSON.stringify(job))
+
+    const applied = await alreadyApplied(
+        session.user.id,
+        context.params.id,
+        prisma
+    )
 
   return {
     props: {
       job,
+      applied,
     },
   }
 }
