ca237f48a8162fbc342c88e412f78b2a5a0ecffd
grandpoisson <grandpoisson@gmail.com>
2022-07-02 13:25:21 +0100

    show applicants in company dashboard

--- components/Jobs.js
+++ components/Jobs.js
@@ -1,12 +1,12 @@
 import Job from 'components/Job'
 
-const Jobs = ({ jobs, isDashboard }) => {
+const Jobs = ({ jobs }) => {
   if (!jobs) return null
 
   return (
     <>
       {jobs.map((job, index) => (
-        <Job key={index} job={job} isDashboard={ isDashboard } />
+        <Job key={index} job={job} />
       ))}
     </>
   )
